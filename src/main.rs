b325f40baed2d96f865597f76c8f457f22a92a53
Andrew (ANB) <andrew.bizyaev@gmail.com>
2021-09-29 22:31:27 +0300

    arrays

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
+#![warn(clippy::all, clippy::pedantic)]
 use std::io::stdin;
 
-fn main() {
-    println!("Hi, your name please!");
-    let mut your_name = String::new();
+fn get_name() -> String {
+    let mut name = String::new();
     stdin()
-        .read_line(&mut your_name)
+        .read_line(&mut name)
         .expect("Failed to read line");
-    println!("Hello, {}", your_name)
+    name
+        .trim()
+        .to_lowercase()
+}
+
+fn main() {
+    let visitor_list = ["andrew", "anb", "slot"];
+    let mut allow_them_in = false;
+    println!("Hi, your name please!");
+    let name = get_name();
+    for visitor in &visitor_list{
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+    if allow_them_in {
+        println!("Hello, {}", name);
+    } else {
+        println!("Sorry, you aren't in the list");
+    }
+
 }
